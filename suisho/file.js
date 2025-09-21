8b14f0917b499e9059c53817a4b510fd69facbb2
yuzu875641 <f.kokori85@gmail.com>
2025-09-21 13:39:17 +0900

    Create file.js

---
+++ suisho/file.js
@@ -0,0 +1,30 @@
+const FormData = require('form-data');
+const axios = require('axios');
+const CHATWORK_API_TOKEN = process.env.CHATWORK_API_TOKEN;
+
+async function sendmiaq(roomId, imageBuffer) {
+  try {
+    const formData = new FormData();
+    formData.append('file', imageBuffer, {
+      filename: `quote.png`,
+      contentType: 'image/png',
+    });
+
+    const uploadUrl = `https://api.chatwork.com/v2/rooms/${roomId}/files`;
+    const headers = {
+      ...formData.getHeaders(),
+      'x-chatworktoken': CHATWORK_API_TOKEN,
+    };
+
+    await axios.post(uploadUrl, formData, { headers });
+
+  } catch (error) {
+    console.error('ファイルアップロードエラー:', error.message);
+    if (error.response) {
+      console.error('APIレスポンス:', error.response.data);
+    }
+    throw new Error('ファイル送信に失敗しました。');
+  }
+}
+
+module.exports = { sendmiaq };
